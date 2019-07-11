f1ccab63527752efcb8dfb2d83a63fb86a4c4eb2
Sonia Ionescu <sonia-adeline@live.com>
2019-07-11 00:23:57 -0700

    Create twoStrings.java

---
+++ twoStrings.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        String shareSubset = "NO";
+        Set<Character> unique1 = new HashSet<Character>();
+        for(char c : s1.toCharArray()){
+            unique1.add(c);
+        } 
+        for(char d : s2.toCharArray()){
+            if(unique1.contains(d) == true){
+                shareSubset = "YES";
+                break;
+            }
+        }
+        return shareSubset;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
